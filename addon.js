3ba6408dc3589738932ca6cadb85c6c1dea75b98
ThatOneGuyWhoLoveCode <116115199+ThatOneGuyWhoLoveCode@users.noreply.github.com>
2023-07-30 22:36:52 +0200

    Create addon.js

---
+++ addon.js
@@ -0,0 +1,13 @@
+/**
+ * @name ExampleAddon
+ * @author YourName
+ * @description Describe the basic information. Maybe a support server link.
+ * @version 0.0.1
+ * @invite inviteCode
+ * @authorId 51512151151651
+ * @authorLink https://twitter.com/Whoever
+ * @donate https://paypal.me/
+ * @patreon https://patreon.com/
+ * @website https://github.com/BetterDiscord/BetterDiscord
+ * @source https://gist.github.com/rauenzi/e5f4d02fc3085a53872b0236cd6f8225
+ */
